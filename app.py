43d1e4c4cc7e533fe2fb9f25777af5b9a75eb21d
DarkCoder26 <132475572+DarkCoder26@users.noreply.github.com>
2023-05-11 16:49:08 +0530

    made grayscale method and requirement.txt

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from werkzeug.utils import secure_filename
+import os
+import cv2
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'webp', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'Super secret key'
+
+def processImage(filename, operation):
+    img = cv2.imread(f"uploads/{filename}")
+    match operation:
+        case 'cgray':
+            imgProcessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(f"static/{filename}", imgProcessed)
+            return filename
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def home():
@@ -8,8 +28,21 @@ def home():
 
 @app.route('/edit', methods=["GET", "POST"])
 def edit():
-    if request.method == "POST":
-        return "it worked"
+    operation = request.form.get('Operation')
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return "Error no file found."
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return "Error no file found."
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            processImage(filename, operation)
+            flash(f"Your image is process and is available <a href='/static/{filename}'>here</a>")
+            return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
