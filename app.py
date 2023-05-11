6a42b40c5ae5d458446ce4023414ab572388f558
DarkCoder26 <132475572+DarkCoder26@users.noreply.github.com>
2023-05-11 13:47:03 +0530

    made summit form and pulled request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/edit', methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        return "it worked"
+
 if __name__ == '__main__':
-    print('working')
     app.run(host='0.0.0.0', debug=True)
